952e32bdd8bd6be020372931de4f6b2b63e5316f
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2025-10-15 08:41:03 -0300

    Update version to 4.2.0 and document changes in changelog

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@
 """
 Setup script for pipe-vms-research
 """
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='pipe-vms-research',
-    version='4.1.1',
+    version='4.2.0',
     packages=find_packages(exclude=['test*.*', 'tests']),
     include_package_data=True
 )
